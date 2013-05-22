27490133f349692ba00951f9ed503326eed9a6db
Dean Okamura <dokamura@us.ibm.com>
2013-05-21 17:52:32 -0700

    clock

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,50 @@
 #
-# Cookbook Name:: itmhost
-# Recipe:: default
+# Licensed Materials - Property of IBM
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# 2013-IBM
 #
-# All rights reserved - Do Not Redistribute
+# (C) Copyright IBM Corp. 2013    All Rights Reserved.
 #
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with
+# IBM Corp.
+#
+include_recipe 'resolver'
+
+execute "git user" do
+    command "printf '[user]\n    name = Dean Okamura\n    email = dokamura@us.ibm.com\n' >> /home/vagrant/.gitconfig"
+    creates "/home/vagrant/.gitconfig"
+end
+
+directory "/home/vagrant/temp" do
+    owner "vagrant"
+    group "vagrant"
+    mode 0775
+    action :create
+end
+
+link "/home/vagrant/cmvc" do
+    owner "vagrant"
+    group "vagrant"
+    to "/Users/dokamura/cmvc"
+end
+
+link "/home/vagrant/etc" do
+    owner "vagrant"
+    group "vagrant"
+    to "/Users/dokamura/etc"
+end
+
+link "/home/vagrant/tms630fp2" do
+    owner "vagrant"
+    group "vagrant"
+    to "/Users/dokamura/tms630fp2"
+end
+
+yum_package "git" do
+  action :install
+end
+
+yum_package "mksh" do
+  action :install
+end
